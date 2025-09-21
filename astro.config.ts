5f3918ac827526b50ee5256715d7068b7eee0b1e
Haris Raharjo Putro <haris.r.putro@gmail.com>
2025-09-21 13:10:53 +0700

    add projects

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
     ? 'https://astro-shadcn-ui-template.vercel.app'
     : 'http://localhost:4321',
-  integrations: [
-    react(),
-  ],
+  integrations: [react(), icon()],
 })
