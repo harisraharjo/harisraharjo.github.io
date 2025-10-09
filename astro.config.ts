80273e858849f59910289a54b32b4502629546c9
Haris Raharjo Putro <haris.r.putro@gmail.com>
2025-10-09 14:02:55 +0700

    fix ci, package.json

--- astro.config.ts
+++ astro.config.ts
@@ -3,10 +3,17 @@ import react from '@astrojs/react'
 
 import icon from 'astro-icon';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
     ? 'https://harisraharjo.github.io'
     : 'http://localhost:4321',
+
   integrations: [react(), icon()],
-}) 
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
